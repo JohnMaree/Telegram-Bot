9d7c4d20f5fe0376ccf1288bbbd842e6893d3e6c
JohnMaree <johnmaree24@gmail.com>
2018-12-15 16:28:42 +0200

    .

---
+++ NodeJS/my_headers/textTemplates.js
@@ -0,0 +1,18 @@
+//File where lengthy responses i.e. constant templates live
+
+
+var helpText = `Hi! I am SonarrBot! \u{1F603}.
+
+My sole purpose in life is making your life easier by interfacing with Sonarr.
+
+Here are some things I can do.
+
+Commands:
+- /search {TV-Show you want} :: Use this to search and add a new TV-Show.
+- /calendar :: will get yesterday's, today's and tomorrow's aired/airing shows.
+- /help :: Gives you what you're reading
+
+Enjoy! \u{1F44D}
+`;
+
+exports.helpText = helpText;
--- NodeJS/sonarrAPIcalls.js
+++ NodeJS/sonarrAPIcalls.js
@@ -1,7 +1,8 @@
 var express = require('express')
-var app = express()
+var templates = require('./my_headers/textTemplates')
 const axios = require('axios')
 var SonarrAPI = require('sonarr-api');
+var app = express()
 
 BOT_URL = 'https://api.telegram.org/bot763429199:AAGCFRePEp7Hcl5Ij4Wlx5_UW_pCwzO4rP4'
 var sonarrCallbackCache = []
@@ -13,14 +14,12 @@ var yesterday = new Date(today.getFullYear(), today.getMonth(), (today.getDate()
 var tomorrow = new Date(today.getFullYear(), today.getMonth(), (today.getDate()+1));
 var a4tomorrow = new Date(today.getFullYear(), today.getMonth(), (today.getDate()+2));
 
-
 app.use(express.json()) // for parsing application/json
 app.use(
   express.urlencoded({
     extended: true
   })
 )
-
 
 var sonarr = new SonarrAPI({
 		hostname: 'localhost',
@@ -29,48 +28,50 @@ var sonarr = new SonarrAPI({
 		urlBase: '/'
 })
 
-
 //This is the route the API will call -> the URL endpoint thats attatched to a localhost
 app.post('/new-message', function(req, res) {
   const { message } = req.body //gets the json file
 
   //console.log(message)
   //console.log(req.body.callback_query)
+
+  //if theres a call_back query, deal with it. This can be a lot better, and with multiple..
+  //..queries an inspection of the queryID or some unique identifier should be used..
+  //..as this only supports one type of callback
   if (req.body.callback_query) {
-      for (i = 0; i < sonarrCallbackCache.length; i++) {
-        if (Number(req.body.callback_query.data) == sonarrCallbackCache[i].sonarr.tvdbId) {
-          console.log('glorious!')
+    for (i = 0; i < sonarrCallbackCache.length; i++) {
+      if (Number(req.body.callback_query.data) == sonarrCallbackCache[i].sonarr.tvdbId) {
+        console.log('glorious!')
 
-          sonarr.post("series", sonarrCallbackCache[i].sonarr)
-          .then(result => {
+        sonarr.post("series", sonarrCallbackCache[i].sonarr)
+        .then(result => {
 
-            axios
-              .post(
-                BOT_URL + '/sendPhoto',
-                {
-                  chat_id: req.body.callback_query.message.chat.id,
-                  photo:  sonarrCallbackCache[i].extras.posterURL.toString(),
-                  caption: 'Added series ~> ' + sonarrCallbackCache[i].sonarr.title + ' <~'
-                }
-              )
-              .then(response => {
-                console.log('Added ' + sonarrCallbackCache[i].sonarr.title)
-                res.end('Added ' + sonarrCallbackCache[i].sonarr.title)
-                sonarrCallbackCache = [] //clear cache
-              })
-              .catch(err => {
-                console.log('Error: ', err)
-                return res.end('Error: ' + err)
-              })
+          axios.post(
+            BOT_URL + '/sendPhoto',
+            {
+              chat_id: req.body.callback_query.message.chat.id,
+              photo:  sonarrCallbackCache[i].extras.posterURL.toString(),
+              caption: 'Added series ~> ' + sonarrCallbackCache[i].sonarr.title + ' <~'
+            }
+          )
+          .then(response => {
+            console.log('Added: ' + sonarrCallbackCache[i].sonarr.title)
+            res.end('ok')
+            sonarrCallbackCache = [] //clear cache
           })
           .catch(err => {
-            console.log('Error: ' + err)
-            res.end('Error: ' + err)
+            console.log(err)
+            return res.end(err)
           })
-          return res.end('callback')
-        }
+        })
+        .catch(err => {
+          console.log(err)
+          res.end(err)
+        })
+        return res.end('ok')
       }
     }
+  }
 
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
   if (!message || !message.text || message.sticker) {
@@ -85,7 +86,7 @@ app.post('/new-message', function(req, res) {
       console.log("blank")
       return res.end('blank input')
 
-    } else {
+    } else { ///
 
     sonarr.get("series/lookup", { "term": message.text.toLowerCase().slice(8) })
       .then(function (result) { //result of get is a json
@@ -94,7 +95,6 @@ app.post('/new-message', function(req, res) {
       var inLineButtonArray = []
       var inLineButtonSubArray = []
       var buttonCounter = 1
-
 
       for (var title in result) {
 
@@ -126,7 +126,6 @@ app.post('/new-message', function(req, res) {
         console.log(buttonObj)
         console.log(callbackObj)
 
-
         if (buttonCounter%4 != 0) {
           inLineButtonSubArray.push(buttonObj)
         } else {
@@ -139,34 +138,33 @@ app.post('/new-message', function(req, res) {
 
       inLineButtonArray.push(inLineButtonSubArray)
 
-      console.log(msgText)
+      //console.log(msgText)
 
-      axios
-        .post(
-          BOT_URL + '/sendMessage',
-          {
-            chat_id: message.chat.id,
-            text: msgText,
-            reply_markup: {
-              inline_keyboard: inLineButtonArray
-            }
+      axios.post(
+        BOT_URL + '/sendMessage',
+        {
+          chat_id: message.chat.id,
+          text: msgText,
+          reply_markup: {
+            inline_keyboard: inLineButtonArray
           }
-        )
-        .then(response => {
-          console.log('Lookup')
-          res.end('lookup')
-        })
-        .catch(err => {
-          console.log('Error :', err)
-          res.end('Error :' + err)
-        })
+        }
+      )
+      .then(response => {
+        console.log('searching...')
+        res.end('ok')
+      })
+      .catch(err => {
+        console.log(err)
+        res.end(err)
+      })
     })
     .catch(err => {
-      console.log('Error: ', err)
-      res.end('error')
+      console.log(err)
+      res.end(err)
     })
   }
-
+  ///
 } else if ((message.text.toLowerCase().indexOf('/calendar') !== -1) == true) {
 
   var yesterdayMsg = "Yesterday:\n"
@@ -186,122 +184,57 @@ app.post('/new-message', function(req, res) {
           yesterdayMsg += '\u{1F4FA}' + '  ' + result[recent].series.title + ' - ' + result[recent].title + '\n'
         } else if (result[recent].airDateUtc.slice(8,10) == (today.getDate())) {
           todayMsg += '\u{1F4FA}' + '  ' + result[recent].series.title + ' - ' + result[recent].title + '\n'
-          console.log(result[recent].airDateUtc.slice(8,10))
         } else if (result[recent].airDateUtc.slice(8,10) == (tomorrow.getDate())) {
           tomorrowMsg += '\u{1F4FA}' + '  ' + result[recent].series.title + ' - ' + result[recent].title + '\n'
-          console.log('tomorrow ' + result[recent].airDateUtc.slice(8,10))
         }
-
       }
+
       console.log(yesterdayMsg + '\n' + todayMsg + '\n' + tomorrowMsg)
 
-      axios
-        .post(
+      axios.post(
           BOT_URL + '/sendMessage',
           {
             chat_id: message.chat.id,
-            text: (yesterdayMsg + '\n\n' + todayMsg + '\n\n' + tomorrowMsg)
+            text: (yesterdayMsg + '\n' + todayMsg + '\n' + tomorrowMsg)
           }
-        )
-        .then(response => {
-          res.end('ok')
-        })
-        .catch(err => {
-          console.log('Error :', err)
-          return res.end('Error :' + err)
-        })
-
+      )
+      .then(response => {
+        res.end('ok')
+      })
+      .catch(err => {
+        console.log(err)
+        return res.end(err)
+      })
     res.end('ok') //important line
     }
   })
   .catch(err => {
-    console.log("Error: " + err)
+    console.log(err)
+    res.end(err)
   })
 
   } else if ((message.text.toLowerCase().indexOf('/help') !== -1) == true) {
-    axios
-      .post(
+
+    axios.post(
         BOT_URL + '/sendMessage',
         {
           chat_id: message.chat.id,
-          text: 'Hi, I am a bot...\nWhat can I help you with?'
+          text: templates.helpText
         }
       )
       .then(response => {
-        console.log('Command Posted')
-        res.end('cmd')
+        console.log('help')
+        res.end('ok')
       })
       .catch(err => {
         // ...and here if it was not
-        console.log('Error :', err)
-        return res.end('Error :' + err)
+        console.log(err)
+        return res.end(err)
       })
-
-  } else if ((message.text.toLowerCase().indexOf('status') !== -1) == true) {
-    // get Sonarr Status
-    sonarr.get("system/status").then(result => {
-     	// result will be json a response
-      console.log(result.version);
-
-      axios
-        .post(
-          BOT_URL + '/sendMessage',
-          {
-            chat_id: message.chat.id,
-            text: result//'Getting status'
-          }
-        )
-        .then(response => {
-          console.log('Getting Status')
-          res.end('sonarr')
-        })
-        .catch(err => {
-          // ...and here if it was not
-          console.log('Error :', err)
-          return res.end('Error :' + err)
-        })
-
-    }).catch(err => {
-    	throw new Error("There was a error processing the request: " + err)
-    })
-
-    } else if ((message.text.toLowerCase().indexOf('marco') !== -1) == false) {
-    axios
-      .post(
-        BOT_URL + '/sendMessage',
-        {
-          chat_id: message.chat.id,
-          text: 'Invalid!!'
-        }
-      )
-    return res.end('not ok')
-  } else {
-
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  //axios.post(URL, JSON)
-  axios
-    .post(
-      BOT_URL + '/sendMessage',
-      {
-        chat_id: message.chat.id,
-        text: 'Polo!!'
-      }
-    )
-    .then(response => {
-      // We get here if the message was successfully posted
-      console.log('Message posted')
-      res.end('ok')
-    })
-    .catch(err => {
-      // ...and here if it was not
-      console.log('Error :', err)
-      res.end('Error :' + err)
-    })
   }
 })
 
 // Finally, start our server
-app.listen(3000, function() {
+app.listen(8181, function() {
   console.log('Telegram app listening on port 3000!')
 })
